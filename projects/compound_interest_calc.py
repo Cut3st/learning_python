c464c90932ef008c7c6cb5b7eed04c1a1202faa3
Danny <dannyhuang1212@gmail.com>
2025-06-18 00:28:40 +0800

    pending completion

---
+++ projects/compound_interest_calc.py
@@ -0,0 +1,9 @@
+#Python compound interest calculator
+#A = P * (1 + r/n)**t
+#A = Final Amount
+#P = Initial principal balance
+#r = Interest rate 
+#t = number of time period elapsed (years) 
+#n = how often is interest compound (monthly, quarterly, yearly)
+
+while principle 
