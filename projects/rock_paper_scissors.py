4df9cfb2875fd382d1aa9c4e4aea8b45986858d0
Danny <dannyhuang1212@gmail.com>
2025-07-04 16:52:54 +0800

    rock paper scissors game with option for the user to play again

---
+++ projects/rock_paper_scissors.py
@@ -0,0 +1,39 @@
+import random
+#Best of three rock paper scissors
+options = ("rock", "paper", "scissors")
+while True:
+    wins = 0
+    losses = 0
+    round = 1
+    player = None
+    while wins < 2 and losses < 2:
+        option = random.choice(options)
+        print()
+        guess = input("ROCK✊, PAPER🖐️, SCISSORS✌️? ").lower()
+        print()
+        if guess == option:
+           print(f"🤝 YOU DRAW ROUND {round}")
+        elif guess == "rock" and option == "scissors":
+            print(f"✅ YOU WIN ROUND {round}.")
+            wins += 1
+        elif guess == "paper" and option == "rock":
+            print(f"✅ YOU WIN ROUND {round}.")
+            wins += 1
+        elif guess == "scissors" and option == "paper":
+            print(f"✅ YOU WIN ROUND {round}.")
+            wins += 1
+        else:
+            print(f"❌ YOU LOST ROUND {round}.")
+            losses += 1
+        print()
+        print(f"🙂 PLAYER: {guess}")
+        print(f"🤖 COMPUTER: {option}\n")
+        round += 1
+    if wins == 2:
+        print("🎊 YOU WIN THE BEST OF 3!")
+    elif losses == 2:
+        print("😿 YOU LOST THE BEST OF 3!")
+    cont = input("Would you want to play another round? (Y/N): ").upper()
+    if cont != "Y":
+        break
+    
