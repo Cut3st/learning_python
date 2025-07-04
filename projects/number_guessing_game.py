7d8a3c2af617a1f20f58eede6810a2b5f03bd1e5
Danny <dannyhuang1212@gmail.com>
2025-07-04 15:44:34 +0800

    user can now choose to play again or quit

--- projects/number_guessing_game.py
+++ projects/number_guessing_game.py
@@ -2,24 +2,29 @@ import random
 #User will guess the randomly generated number between 1 - 100
 low = 1
 high = 100
-answer = random.randint(low,high)
-guesses = 0
-
+#Makes it so the random number can be regenerated for user to play over and over
 while True:
-    guesses += 1
-    guess = int(input("Guess the random number between 1 - 100: "))
-    if guess < low or guess > high:
-        print()
-        print(f"Your guess is not within range please guess within {low} and {high}.\n")
-    elif guess < answer:
-        print()
-        print("📉 Your guess is too low try again!\n")
-    elif guess > answer:
-        print()
-        print("📈 Your guess is too high try again!\n")    
-    else:
-        print()
-        print(f"✅ {answer}! YOU'VE GUESS CORRECTLY WELL DONE!")
-        print(f"You guessed {guesses} times before getting it right!")
+    answer = random.randint(low,high)
+    guesses = 0
+
+    while True:
+        guesses += 1
+        guess = int(input("Guess the random number between 1 - 100: "))
+        if guess < low or guess > high:
+            print()
+            print(f"Your guess is not within range please guess within {low} and {high}.\n")
+        elif guess < answer:
+            print()
+            print("📉 Too low try again!\n")
+        elif guess > answer:
+            print()
+            print("📈 Too high try again!\n")    
+        else:
+            print()
+            print(f"✅ {answer}! YOU'VE GUESS CORRECTLY WELL DONE!")
+            print(f"You guessed {guesses} times before getting it right!")
+            break
+    play_again = input("Would you like to play again? (Y/N):" ).upper()
+    if play_again != "Y":
         break
     
