a6c5f5b673f72a9f300c2bf9cafcb1efd2e5c9f8
Danny <dannyhuang1212@gmail.com>
2025-07-04 15:38:59 +0800

    some visual changes

--- projects/number_guessing_game.py
+++ projects/number_guessing_game.py
@@ -9,13 +9,17 @@ while True:
     guesses += 1
     guess = int(input("Guess the random number between 1 - 100: "))
     if guess < low or guess > high:
-        print(f"Your guess is not within range please guess within {low} and {high}.")
+        print()
+        print(f"Your guess is not within range please guess within {low} and {high}.\n")
     elif guess < answer:
-        print("📉 Your guess is too low try again!")
+        print()
+        print("📉 Your guess is too low try again!\n")
     elif guess > answer:
-        print("📈 Your guess is too high try again!")    
+        print()
+        print("📈 Your guess is too high try again!\n")    
     else:
-        print(f"\n✅{answer}! YOU'VE GUESS CORRECTLY WELL DONE!")
+        print()
+        print(f"✅ {answer}! YOU'VE GUESS CORRECTLY WELL DONE!")
         print(f"You guessed {guesses} times before getting it right!")
         break
     
