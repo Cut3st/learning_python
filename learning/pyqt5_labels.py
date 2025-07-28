391ea66afdbfadbcf8aaa2caf7f41b1de6cb039f
Danny <dannyhuang1212@gmail.com>
2025-07-28 17:40:47 +0800

    learning

---
+++ learning/pyqt5_labels.py
@@ -0,0 +1,39 @@
+#PyQt5 Introduction
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import Qt
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My First GUI")
+        self.setGeometry(700, 300, 500, 500)
+        self.setWindowIcon(QIcon("server-icon.png"))
+
+        label = QLabel("Hello", self)
+        label.setFont(QFont("Arial", 40))
+        label.setGeometry(0, 0, 500, 100)
+        label.setStyleSheet("color: #292929;"
+                            "background-color: #6fdcf7;"
+                            "font-weight: bold;" 
+                            "font-style: italic;"
+                            "text-decoration: underline;")
+        #label.setAlignment(Qt.AlignTop) VERTICALLY TOP
+        #label.setAlignment(Qt.AlignBottom) VERTICALLY BOTTOM
+        #label.setAlignment(Qt.AlignVCenter) VERTICVALLY CENTER
+        #label.setAlignment(Qt.AlignRight) HORIZONTALLY RIGHT
+        #label.setAlignment(Qt.AlignLeft) HORIZONTALLY LEFT
+        label.setAlignment(Qt.AlignCenter) #HORIZONTALLY CENTER
+        
+        
+        
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
